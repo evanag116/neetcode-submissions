eceba48ba307978e55dec7624c96e8febe587886
Evan Garcia <96553672+evanag116@users.noreply.github.com>
2026-07-05 16:33:07 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 3
+print(type(variable))
+variable = 3.5
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "string"
+print(type(variable))
+variable = [0, 1, 2]
+print(type(variable))
